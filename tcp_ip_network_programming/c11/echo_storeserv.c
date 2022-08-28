56168c043674f3be6bc1d9de24d0766e766a01f7
guanyitong <2969413251@qq.com>
2022-08-28 20:05:44 +0800

    nothing

--- tcp_ip_network_programming/c11/echo_storeserv.c
+++ tcp_ip_network_programming/c11/echo_storeserv.c
@@ -21,15 +21,80 @@ int main(int argc, char *argv[]) {
     char buf[BUF_SIZE];
     if (argc != 2) {
         printf("Usage : %s <port>\n", argv[0]);
-        eixt(1);
+        exit(1);
     }
 
     act.sa_handler = read_childproc;
-    sigempty(&act.sa_mask);
+    sigemptyset(&act.sa_mask);
     act.sa_flags = 0;
     state = sigaction(SIGCHLD, &act, 0);
 
-    serv_sock = socket
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = INADDR_ANY;
+    serv_adr.sin_port = htons(atoi(argv[1]));
 
-    
+    if (bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1) {
+        error_handling("bind() error");
+    }
+
+    if (listen(serv_sock, 5) == -1) {
+        error_handling("listen() error");
+    }
+
+    pipe(fds);
+    pid = fork();
+    if (pid == 0) {
+        FILE * fp = fopen("echomsg.txt", "wt");
+        char msgbuf[BUF_SIZE];
+        int i, len;
+
+        for (int i = 0; i < 10; ++i) {
+            len = read(fds[0], msgbuf, BUF_SIZE);
+            fwrite((void*)msgbuf, 1, len, fp);
+        }
+        fclose(fp);
+        return 0;
+    }
+
+    while (1) {
+        adr_sz = sizeof(clnt_adr);
+        clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &adr_sz);
+        if (clnt_sock == -1) {
+            continue;
+        } else {
+            puts("new client connected...");
+        }
+
+        pid = fork();
+        if (pid == 0) {
+            close(serv_sock);
+            while ((str_len = read(clnt_sock, buf, BUF_SIZE)) != 0) {
+                write(clnt_sock, buf, str_len);
+                write(fds[1], buf, str_len);
+            }
+            close(clnt_sock);
+            puts("client disconnected...");
+            return 0;
+        } else {
+            close(clnt_sock);
+        }
+    }
+
+    close(serv_sock);
+    return 0;   
+}
+
+void read_childproc(int sig) {
+    pid_t pid;
+    int status;
+    pid = waitpid(-1, &pid, WNOHANG);
+    printf("remov proc id: %d \n", pid);
+}
+
+void error_handling(char *message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
